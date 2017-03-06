2de0a492962397f2b7df32e4e44a55e9565a0cde
lrg <lrghust@gmail.com>
2017-03-06 12:50:46 +0800

    parallel

--- video.py
+++ video.py
@@ -5,11 +5,12 @@ from fast_rcnn.test import im_detect
 from fast_rcnn.nms_wrapper import nms
 from utils.timer import Timer
 import matplotlib.pyplot as plt
-import numpy as np
+import numpy
 import os, sys, cv2
 import argparse
 from networks.factory import get_network
-
+import copy
+import pp
 
 CLASSES = ('__background__',
            'aeroplane', 'bicycle', 'bird', 'boat',
@@ -23,7 +24,7 @@ CLASSES = ('__background__',
 
 def vis_detections(im, class_name, dets, thresh=0.5):
     """Draw detected bounding boxes."""
-    inds = np.where(dets[:, -1] >= thresh)[0]
+    inds = numpy.where(dets[:, -1] >= thresh)[0]
     if len(inds) == 0:
         return
 
@@ -31,7 +32,7 @@ def vis_detections(im, class_name, dets, thresh=0.5):
         bbox = dets[i, :4]
         score = dets[i, -1]
         cv2.rectangle(im,(int(bbox[0]),int(bbox[1])),(int(bbox[2]),int(bbox[3])),(0,0,255),2)
-        cv2.putText(im,'{:s} {:.3f}'.format(class_name, score),(int(bbox[0]),int(bbox[1])+10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),1,cv2.CV_AA)
+        cv2.putText(im,'{:s} {:.3f}'.format(class_name, score),(int(bbox[0]),int(bbox[1])+15),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),1,cv2.CV_AA)
         '''
         ax.add_patch(
             plt.Rectangle((bbox[0], bbox[1]),
@@ -60,93 +61,65 @@ def demo(sess, net, video_name):
     vc.open(v_file)
     ret,im=vc.read()
     fps=vc.get(5)
-    height = np.size(im, 0)
-    width = np.size(im, 1)
+    height = numpy.size(im, 0)
+    width = numpy.size(im, 1)
     vc.release()
     vc.open(v_file)
     allscores=[]
     allboxes=[]
-    '''
-    while(vc.isOpened()):
+    alldets=[]
+
+    while vc.isOpened():
         ret,im = vc.read()
         if ret is False:
             break
         scores, boxes = im_detect(sess, net, im)
         allscores.append(scores)
         allboxes.append(boxes)
-        ###
-        print 'finish a frame'
-        ###
-    '''
-    for i in range(6):
-        ret,im=vc.read()
-        scores, boxes = im_detect(sess, net, im)
-        allscores.append(scores)
-        allboxes.append(boxes)
         print 'finish a frame'
 
     vc.release()
 
-    CONF_THRESH = 0.5
+    CONF_THRESH = 0.9
     NMS_THRESH = 0.3
-    LINK_THRESH = 0.8
-    alldets=[]
+    LINK_THRESH = 0.6
+    ppservers=()
+    job_server=pp.Server(ppservers=ppservers)
     for cls_ind, cls, in enumerate(CLASSES[1:]):
         cls_ind += 1
         dets=[]
         links=[]
         for index in range(1, len(allboxes)):
-            box1 = allboxes[index - 1]
-            box2 = allboxes[index]
-            score1 = allscores[index - 1]
-            score2 = allscores[index]
-            cls_boxes = box1[:, 4*cls_ind:4*(cls_ind + 1)]
-            cls_scores = score1[:, cls_ind]
-            dets1 = np.hstack((cls_boxes,cls_scores[:, np.newaxis])).astype(np.float32)
-            if index == 1:
-                dets.append(dets1)
-            cls_boxes = box2[:, 4 * cls_ind:4 * (cls_ind + 1)]
-            cls_scores = score2[:, cls_ind]
-            dets2 = np.hstack((cls_boxes, cls_scores[:, np.newaxis])).astype(np.float32)
-            dets.append(dets2)
-            link={}
-            for ind1,box1 in enumerate(dets1):
-                link[ind1]=[]
-                for ind2,box2 in enumerate(dets2):
-                    x1=max(box1[0],box2[0])
-                    y1=max(box1[1],box2[1])
-                    x2=min(box1[2],box2[2])
-                    y2=min(box1[3],box2[3])
-                    area1=(box1[2]-box1[0]+1)*(box1[3]-box1[1]+1)
-                    area2 = (box2[2] - box2[0] + 1) * (box2[3] - box2[1] + 1)
-                    w = max(0.0, x2 - x1 + 1)
-                    h = max(0.0, y2 - y1 + 1)
-                    inter = w * h
-                    ovr = inter / (area1 + area2 - inter)
-                    #print ovr
-                    if ovr >= LINK_THRESH:
-                        link[ind1].append(ind2)
+            job=job_server.submit(computeLinks,(index,cls_ind,allboxes,allscores,LINK_THRESH),(),("numpy",))
+            link,dets1,dets2=job()
             links.append(link)
+            if index==1:
+                dets.append(dets1)
+            dets.append(dets2)
         #start from here-findmaxpath,rescore,remove
         #print links
         #exit()
         links.append({})
+        print 'finish links'
         while True:
-            rootindex,maxpath,maxsum=findMaxPath(links,dets)
+            job=job_server.submit(findMaxPath,(links,dets),(findMaxPathFromOneRoot,getMaxSet,setMaxSet),("copy",))
+            rootindex,maxpath,maxsum=job()
+            #print rootindex,maxpath,maxsum
             if len(maxpath) <= 1:
                 break
-            rescore(dets,rootindex,maxpath,maxsum)
+            rescore(dets,rootindex,maxpath,maxsum,CONF_THRESH)
             deleteLink(links,rootindex,maxpath)
         alldets.append(dets)
     print 'finish rescoring'
     vc.open(v_file)
     vw=cv2.VideoWriter()
-    vw.open('result.mp4',cv2.cv.FOURCC(*'MJPG'),fps,(width,height))
-    for i in range(6):
+    vw.open('result.avi',cv2.cv.FOURCC(*'MJPG'),fps,(width,height))
+
+    i=0
+    while vc.isOpened():
         ret,im=vc.read()
-        #im = im[:, :, (2, 1, 0)]
-        #fig, ax = plt.subplots(figsize=(12, 12))
-        #ax.imshow(im, aspect='equal')
+        if ret is False:
+            break;
         for ind,dets in enumerate(alldets):
             cls=CLASSES[ind+1]
             onedets=dets[i]
@@ -154,11 +127,45 @@ def demo(sess, net, video_name):
             onedets = onedets[keep, :]
             vis_detections(im, cls, onedets, thresh=CONF_THRESH)
         vw.write(im)
+        print 'finish writing a frame'
+        i+=1
+
     vw.release()
+
+def computeLinks(index,cls_ind,allboxes,allscores,LINK_THRESH):
+    box1 = allboxes[index - 1]
+    box2 = allboxes[index]
+    score1 = allscores[index - 1]
+    score2 = allscores[index]
+    cls_boxes = box1[:, 4*cls_ind:4*(cls_ind + 1)]
+    cls_scores = score1[:, cls_ind]
+    dets1 = numpy.hstack((cls_boxes,cls_scores[:, numpy.newaxis])).astype(numpy.float32)
+    cls_boxes = box2[:, 4 * cls_ind:4 * (cls_ind + 1)]
+    cls_scores = score2[:, cls_ind]
+    dets2 = numpy.hstack((cls_boxes, cls_scores[:, numpy.newaxis])).astype(numpy.float32)
+    link={}
+    for ind1,box1 in enumerate(dets1):
+        link[ind1]=[]
+        for ind2,box2 in enumerate(dets2):
+            x1=max(box1[0],box2[0])
+            y1=max(box1[1],box2[1])
+            x2=min(box1[2],box2[2])
+            y2=min(box1[3],box2[3])
+            area1=(box1[2]-box1[0]+1)*(box1[3]-box1[1]+1)
+            area2 = (box2[2] - box2[0] + 1) * (box2[3] - box2[1] + 1)
+            w = max(0.0, x2 - x1 + 1)
+            h = max(0.0, y2 - y1 + 1)
+            inter = w * h
+            ovr = inter / (area1 + area2 - inter)
+            #print ovr
+            if ovr >= LINK_THRESH:
+                link[ind1].append(ind2)
+    return link,dets1,dets2
 
 #link:dict links:list
 def findMaxPath(links,dets):
     #start from here
+    maxset={}
     delete=[]
     maxpath=[]
     maxsum=0
@@ -172,28 +179,57 @@ def findMaxPath(links,dets):
         delete=[]
         for root in roots:
             delete.extend(link[root])
-            curpath,cursum=findMaxPathFromOneRoot(links,dets,link_ind,root)
+            curpath,cursum=findMaxPathFromOneRoot(links,dets,maxset,link_ind,root)
             if cursum > maxsum:
                 maxpath=curpath
                 maxsum=cursum
                 rootindex=link_ind
     return rootindex,maxpath,maxsum
 
-def findMaxPathFromOneRoot(links,dets,index,root):
+def findMaxPathFromOneRoot(links,dets,maxset,index,root):
     if root not in links[index] or links[index][root]==[]:
+        #print 'findMaxPathFromOneRoot',[root],index
+        #tmp=input(':')
         return [root],dets[index][root][4]
+    ret,maxpath,maxsum=getMaxSet(maxset,index,root)
+    if ret:
+        #print 'getMaxSet',maxpath,index
+        #tmp=input(':')
+        return maxpath,maxsum
     maxsum=0
+    maxroot=0
     for nextroot in links[index][root]:
-        curpath,cursum=findMaxPathFromOneRoot(links,dets,index+1,nextroot)
+        curpath,cursum=findMaxPathFromOneRoot(links,dets,maxset,index+1,nextroot)
         if cursum > maxsum:
             maxsum=cursum
             maxpath=curpath
+            maxroot=nextroot
+    maxpath=copy.copy(maxpath)
+    maxsum=copy.copy(maxsum)
     maxsum+=dets[index][root][4]
     maxpath.insert(0,root)
+    setMaxSet(maxset,index,root,maxpath,maxsum)
+    #if (index+1,maxroot) in maxset:
+    #    maxset.pop((index+1,maxroot))
+    #print 'findMaxPathFromOneRoot',maxpath,index
+    #tmp=input(':')
     return maxpath,maxsum
 
-def rescore(dets, rootindex, maxpath, maxsum):
+def getMaxSet(maxset,index,root):
+    if (index,root) in maxset:
+        maxpath=maxset[(index,root)][0]
+        maxsum=maxset[(index,root)][1]
+        return True,maxpath,maxsum
+    return False,[],0
+
+def setMaxSet(maxset,index,root,maxpath,maxsum):
+    maxset[(index,root)]=[maxpath,maxsum]
+    #print maxset
+
+def rescore(dets, rootindex, maxpath, maxsum,thesh):
     newscore=maxsum/len(maxpath)
+    #if newscore<thesh:
+    #    return
     for i,box in enumerate(maxpath):
         onedets=dets[rootindex+i]
         onedets[box][4]=newscore
@@ -209,8 +245,8 @@ def deleteLink(links, rootindex, maxpath):
                     prior[key].remove(box)
 
 
-def getScore(frame_index, box_index, dets):
-    return dets[frame_index][box_index][4]
+#def getScore(frame_index, box_index, dets):
+#    return dets[frame_index][box_index][4]
 
 def parse_args():
     """Parse input arguments."""
@@ -248,9 +284,9 @@ if __name__ == '__main__':
     print '\n\nLoaded network {:s}'.format(args.model)
 
     # Warmup on a dummy image
-    im = 128 * np.ones((300, 300, 3), dtype=np.uint8)
+    im = 128 * numpy.ones((300, 300, 3), dtype=numpy.uint8)
     for i in xrange(2):
         _, _= im_detect(sess, net, im)
 
-    video='test.mp4'
+    video='test.mov'
     demo(sess,net,video)
